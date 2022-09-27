d928dc8b842c718c5cdcf1c684ee4ebfbfffd435
hayatul-islam <hayatulemon340@gmail.com>
2022-09-27 19:45:49 +0600

    header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,14 @@ import { NavLink, useLocation } from "react-router-dom";
 
 function Header() {
   const { pathname } = useLocation();
+  const handleEmail = () => {
+    window.open("mailto:hello@gmail.com?subject=&body=");
+  };
 
   return (
     <>
-      <div className="grid grid-cols-3 ">
-        <div className="text-[120px] font-medium">
+      <div className="grid grid-cols-2 md:grid-cols-3 ">
+        <div className="text-[65px] md:text-[90px] xl:text-[120px] font-medium">
           <NavLink
             className={({ isActive }) =>
               isActive && pathname === "/"
@@ -30,21 +33,27 @@ function Header() {
             Works
           </NavLink>
         </div>
-        <div className="text-center">
+        <div className="hidden md:block text-center">
           <h2 className="text-[24px] text-[#242b2d] font-medium">
             Selected Works
           </h2>
         </div>
         <div className="text-end">
-          <div className="space-x-4 ">
+          <div className="space-x-4 text-[18px] sm:text-[20px] lg:text-[24px]">
             <motion.a
-              className="underline text-[24px] font-medium"
-              href="/"
+              className="underline font-medium"
+              href="https://www.instagram.com/_shiftwalk.studio/"
               target="_blank"
             >
-              Instagram
+              <span className="hidden md:inline-block underline">
+                Instagram
+              </span>
+              <span className="md:hidden">Insta</span>
             </motion.a>
-            <motion.button className={`underline text-[24px] font-medium `}>
+            <motion.button
+              onClick={handleEmail}
+              className={`underline font-medium `}
+            >
               Email
             </motion.button>
           </div>
