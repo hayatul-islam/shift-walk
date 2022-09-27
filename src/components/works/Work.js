951ca6e3cf09de4c948cdb531e1b35a218f3b41c
hayatul-islam <hayatulemon340@gmail.com>
2022-09-27 19:29:38 +0600

    link add

--- src/components/works/Work.js
+++ src/components/works/Work.js
@@ -5,10 +5,14 @@ import { BsArrowRight } from "react-icons/bs";
 function Work({ data }) {
   const [show, setShow] = useState(false);
   console.log(show);
-  const { client, agency, overview } = data || {};
+  const { client, agency, overview, role, link } = data || {};
+  const handleLink = () => {
+    window.open(link, "_blank");
+  };
   return (
     <>
       <motion.div
+        onClick={handleLink}
         onMouseEnter={() => setShow(true)}
         onMouseLeave={() => setShow(false)}
         className="grid grid-cols-[30%_45%_15%_10%] border-b  border-[#242b2d] py-1 text-[24px] font-medium text-[#242b2d] cursor-pointer items-center"
@@ -17,7 +21,19 @@ function Work({ data }) {
           <h4>{show ? "Launch Project" : client}</h4>
         </motion.div>
         <div>
-          <h4>{overview}</h4>
+          {show ? (
+            <div className="w-[500px]">
+              <marquee>
+                <ul className="flex space-x-8 list-disc list-outside">
+                  {[1, 2, 3, 4, 5, 6].map((li) => (
+                    <li>{role}</li>
+                  ))}
+                </ul>
+              </marquee>
+            </div>
+          ) : (
+            <h4>{overview}</h4>
+          )}
         </div>
         <div>
           <h4>{agency !== "" ? agency : "-"}</h4>
