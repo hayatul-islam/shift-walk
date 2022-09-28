2c4e97b5e01624d8ab745ef273053db6465a7731
hayatul-islam <hayatulemon340@gmail.com>
2022-09-28 13:14:46 +0600

    bio animation add

--- src/components/home/BioData.js
+++ src/components/home/BioData.js
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import React from "react";
+import { contentVarients } from "../../utils/defaultVarients";
 
 function BioData({ data, theme }) {
   const { role, selected, stack, awards } = data || {};
@@ -11,39 +13,67 @@ function BioData({ data, theme }) {
       >
         <div>
           <div
-            className={`flex justify-between border-b border-t  py-1 border-${
+            className={` border-b border-t  py-1 border-${
               theme === "light" ? "[#242b2d]" : "[#abaea8]"
             }`}
           >
-            <h4>Role</h4>
-            <h4>{role}</h4>
+            <motion.div
+              initial={contentVarients.initial}
+              animate={contentVarients?.animate}
+              transition={contentVarients?.transition}
+              className="flex justify-between"
+            >
+              <h4>Role</h4>
+              <h4>{role}</h4>
+            </motion.div>
           </div>
           <div
-            className={`flex justify-between border-b border-t  py-1 border-${
+            className={` border-b border-t  py-1 border-${
               theme === "light" ? "[#242b2d]" : "[#abaea8]"
             }`}
           >
-            <h4>Selected Clients</h4>
-            <h4>{selected}</h4>
+            <motion.div
+              initial={contentVarients.initial}
+              animate={contentVarients?.animate}
+              transition={contentVarients?.transition}
+              className="flex justify-between"
+            >
+              <h4>Selected Clients</h4>
+              <h4>{selected}</h4>
+            </motion.div>
           </div>
           {stack && (
             <div
-              className={`flex justify-between border-b border-t  py-1 border-${
+              className={` border-b border-t  py-1 border-${
                 theme === "light" ? "[#242b2d]" : "[#abaea8]"
               }`}
             >
-              <h4>Stack</h4>
-              <h4>{stack}</h4>
+              <motion.div
+                initial={contentVarients.initial}
+                animate={contentVarients?.animate}
+                transition={contentVarients?.transition}
+                className="flex justify-between"
+              >
+                <h4>Stack</h4>
+                <h4>{stack}</h4>
+              </motion.div>
             </div>
           )}
 
           <div
-            className={`flex justify-between border-b border-t  py-1 border-${
+            className={` border-b border-t  py-1 border-${
               theme === "light" ? "[#242b2d]" : "[#abaea8]"
             }`}
           >
-            <h4>Awards</h4>
-            <h4>{awards}</h4>
+            <motion.div
+              initial={contentVarients.initial}
+              animate={contentVarients?.animate}
+              transition={contentVarients?.transition}
+              className="flex justify-between"
+            >
+              <h4>Awards</h4>
+              <h4>{awards}</h4>
+            </motion.div>
           </div>
         </div>
       </div>
